93e02fea8b8c5ea37a26433db76473f0c153b471
Lukasz Gawron <51411768+LUKIS123@users.noreply.github.com>
2023-04-22 16:19:14 +0200

    round up precision added

--- Lab3/kalkulator.s
+++ Lab3/kalkulator.s
@@ -8,38 +8,46 @@ _start:
     fstcw control
     movw control, %ax
 
-and:
-    andw  $0b1111110011111111, %ax # truncate, czyli do 0
-    # andw    $0b1111000011111111, %ax 
-    # orw     $0b0000010000000000, %ax # round down
+prec:
+    #andw  $0b1111110011111111, %ax      # set precision to single
+    #orw    $0b0000110000000000, %ax     # truncate, czyli round towards zero
+    
+    #andw    $0b1111000011111111, %ax
+    #orw     $0b0000010000000000, %ax   # round down
+
+    andw    $0b1111000011111111, %ax 
+    orw     $0b0000100000000000, %ax   # round up
+
     movw %ax, control
 
 cl:
     fldcw control       # zaladowanie slowa sterjacego do rejestru sterujacego
 
 cs
-    fstcw control       # dczytane do bufora w pamieci rozkazem fstcw zeby mod sprawdzic
+    fstcw control       # odczytane do bufora w pamieci rozkazem fstcw zeby sprawdzic control word
 
-set:
+stload:
 
     flds liczba1
     flds liczba2
 
 calc:
+    # fmul %st(1), %st(0)
 
-    fmul %st(1), %st(0)
-    # fmulp %st, %st(1) # wariant czysci st(1), wynik na st(0)
+    # fmulp %st, %st(1)           # wariant czysci st(1), wynik na st(0)
 
-    # fdivp # dzieli i wrzuca wynik na stos st (0)
+    fdivp %st, %st(1)           # dzieli liczba2/liczba1 i wrzuca wynik na stos st (0)
 
     # faddp %st, %st(1)           # dodaje i wrzuca wynik na stos st (0)
 
     # fsubrp %st(0), %st(1)       # liczba1 - liczba2
 
+    # fsqrt                       # sqrt z liczba2 do wygenerowania NaN
+
 koniec:
-    mov $SYSEXIT, %eax          # wywołanie systemowego wyjścia
-    mov $EXIT_SUCCESS, %ebx     # kod powrotu
-    int $0x80                   # przerwanie systemowe
+    mov $SYSEXIT, %eax
+    mov $EXIT_SUCCESS, %ebx
+    int $0x80
 
 .data
     SYSEXIT = 1
@@ -49,14 +57,22 @@ control:
     .word 0
 
 liczba1:
-    .float 1.2
+    .float 1.0
     
 liczba2:
-    .float 1.6
+    .float 0.00000000000000000000000000000000000001 
 
 # komendy:
 # p $st1
 # p/t $st1
 # p/t (short) control -> do sprawdzenia ustawien precyzji
 # p/t $fstat -> do sprawdzenia flag
-# info all-registers 
+# info all-registers
+
+# wyjatki:
+# +INF 1.0/0.0
+# -INF -1.0/0.0
+# NaN 0.0/0.0 albo sqrt -1.0
+# Zdenormalizowana 0.00000000000000000000000000000000000001 i patrzymy do fstat p/t $fstat bit [1] ustawiony na 1 oznacza zdenormalizowana
+# +0 odejmujemy dowolne takie same wartosci albo 1 * 0
+# -0 mnozymy -1 * 0
